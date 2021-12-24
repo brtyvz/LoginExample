a3e3b07a8a968c82840451517a4fb454b5b77baa
brtyvz <beratttyavuzz@gmail.com>
2021-12-24 19:02:57 +0300

    commit

--- LoginExample/ViewController.swift
+++ LoginExample/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldKullaniciAdi: UITextField!
+    @IBOutlet weak var textFieldSifre: UITextField!
+    
+    let d = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //eğer kullanıcı adı ve sifre değerlei bos değerinden farklıysa direkt öbür sayfaya geçiş yap
+        let ka = d.string(forKey: "kullaniciAdi") ?? "bos"
+        let s = d.string(forKey: "sifre") ?? "bos"
+        
+        if ka != "bos" && s != "bos"  {
+            performSegue(withIdentifier: "girisToAnasayfa", sender: nil)
+        }
     }
 
-
+    @IBAction func girisYap(_ sender: Any) {
+    
+        if let ka = textFieldKullaniciAdi.text,let s = textFieldSifre.text{
+            
+            if s == "123456" && ka == "admin"{
+                d.setValue(ka, forKey: "kullaniciAdi")
+                d.setValue(s, forKey: "sifre")
+                performSegue(withIdentifier: "girisToAnasayfa", sender: nil)
+            }
+            else{print("hatalı giriş")}
+            
+            
+        }
+    
+    
+    }
+    
+    
+    // app çalıştığında nav controller gizlenir
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    //başka sayfaya geçtiğinde nav tekrar görünür olur
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
 }
 
